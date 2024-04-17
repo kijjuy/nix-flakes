916830ebdf571256823812267b539b6d93e8f2ce
kijjuy <brandonzomer@proton.me>
2024-04-17 14:58:07 -0400

    add python flake

--- python/flake.nix
+++ python/flake.nix
@@ -1,15 +1,26 @@
 {
-  description = "A very basic flake";
+  description = "Python development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          flask
+          # Add more Python packages here as needed
+        ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            # Add more build inputs here as needed
+          ];
+        };
+      }
+    );
 }
