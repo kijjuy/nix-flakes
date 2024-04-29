ec22f245dc177f65377756b54bda906a76f90262
kijjuy <brandonzomer@proton.me>
2024-04-29 15:31:58 -0400

    fix typo

--- go/flake.nix
+++ go/flake.nix
@@ -9,7 +9,7 @@
   outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPacakges.${system};
+        pkgs = nixpkgs.legacyPackages.${system};
       in {
 	devShell = pkgs.mkShell {
 	  buildInputs = with pkgs; [
